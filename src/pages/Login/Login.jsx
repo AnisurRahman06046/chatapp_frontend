5afdde2183792bb9622d262f3422e3ae28ba165d
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-07-03 17:35:33 +0600

    login page is done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,40 @@ function Login() {
           Login
           <span className="text-blue-500"> ChatApp</span>
         </h1>
+
+        {/* form section */}
+        <form action="">
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter you password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <a
+          href="#"
+          className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block "
+        >
+          {"Don't"} have an account?
+        </a>
+        <div>
+            <button className="btn btn-block btn-sm mt-2">Login</button>
+        </div>
+        </form>
+
       </div>
     </div>
   );
