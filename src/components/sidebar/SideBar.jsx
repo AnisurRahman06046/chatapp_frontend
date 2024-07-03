08766ff3f732bf1b6a480010cac80dc82d7a7f75
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-07-03 22:00:59 +0600

    logout btn is added

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,16 +1,19 @@
 import Converations from "./Converations"
+import LogOutBtn from "./LogOutBtn"
 import SearchInput from "./SearchInput"
 
 
 function SideBar() {
   return (
-    <div>
+    <div className="border-r border-slate-500 p-4 flex flex-col">
         {/* search bar  component*/}
         <SearchInput/>
         <div className="divider px-3"></div>
         {/* Conversations component */}
         <Converations/>
         {/* logout component */}
+        <LogOutBtn/>
+
     </div>
   )
 }
