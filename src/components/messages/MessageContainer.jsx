1b593bc3cda12d4e7715ab2b4c64d169b9f6aebd
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-07-03 22:47:29 +0600

    msg body structure is done

---
+++ src/components/messages/MessageContainer.jsx
@@ -0,0 +1,20 @@
+import Messages from "./Messages";
+
+function MessageContainer() {
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      <>
+        {/* header */}
+        <div className="bg-slate-500 px-4 py-2 mb-2 text-left">
+          <span className="label-text">To:</span>{" "}
+          <span className="text-gray-900 font-bold">John Doe</span>
+        </div>
+        {/* messages component */}
+        <Messages/>
+        {/* message input */}
+      </>
+    </div>
+  );
+}
+
+export default MessageContainer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
+import MessageContainer from "../../components/messages/MessageContainer";
 import SideBar from "../../components/sidebar/SideBar";
 
 function Home() {
-  return <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-    {/* sidebar component */}
-    <SideBar/>
-    {/* message container  */}
-  </div>;
+  return (
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      {/* sidebar component */}
+      <SideBar />
+      {/* message container  */}
+      <MessageContainer />
+    </div>
+  );
 }
 
 export default Home;
