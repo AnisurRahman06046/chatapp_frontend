e928aab43ff5bdce7432604f326c5b63e8c63ac7
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-07-04 16:56:05 +0600

    send message is done

---
+++ src/hooks/useSendMessage.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import useConversation from "../store/useConversation";
+
+const useSendMessage = () => {
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+  const token = localStorage.getItem("access_token");
+  const sendMessage = async (message) => {
+    setLoading(true);
+    const payload = {
+      message: message,
+    };
+    try {
+      const res = await fetch(
+        `http://localhost:3000/messages/sendMessage/${selectedConversation._id}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+      const data = await res.json();
+    //   console.log(data);
+      if (data.statusCode === 200) {
+        setMessages([...messages, data.message]);
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { sendMessage, loading };
+};
+
+export default useSendMessage;
