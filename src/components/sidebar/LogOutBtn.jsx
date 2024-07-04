995b9d185596bf4e77790bf9fecca10d9aee3901
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-07-04 12:42:57 +0600

    logout done

--- src/components/sidebar/LogOutBtn.jsx
+++ src/components/sidebar/LogOutBtn.jsx
@@ -1,12 +1,20 @@
-import { BiLogOut } from "react-icons/bi"
-
+import { BiLogOut } from "react-icons/bi";
+import { useAuthContext } from "../../context/AuthContext";
 
 function LogOutBtn() {
+  const { logout } = useAuthContext();
+  const handleLogout = () => {
+    // console.log("logout");
+    logout();
+  };
   return (
     <div className="mt-auto">
-        <BiLogOut className="w-6 h-6 text-white cursor-pointer"/>
+      <BiLogOut
+        onClick={handleLogout}
+        className="w-6 h-6 text-white cursor-pointer"
+      />
     </div>
-  )
+  );
 }
 
-export default LogOutBtn
+export default LogOutBtn;
